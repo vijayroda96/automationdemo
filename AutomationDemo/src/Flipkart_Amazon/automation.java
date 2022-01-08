82e68b205f7e12ce30d8d7816e62980304c45972
viju <viju@DESKTOP-EO0JQ99.mshome.net>
2022-01-08 09:48:21 +0530

    Automation practiced

--- AutomationDemo/src/Flipkart_Amazon/automation.java
+++ AutomationDemo/src/Flipkart_Amazon/automation.java
@@ -36,7 +36,7 @@ public class automation {
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
+    //driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
--- AutomationDemo/src/Flipkart_Amazon/opencart.java
+++ AutomationDemo/src/Flipkart_Amazon/opencart.java
@@ -43,7 +43,7 @@ public class opencart {
 
 	  @AfterClass(alwaysRun = true)
 	  public void tearDown() throws Exception {
-	    driver.quit();
+	    //driver.quit();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
 	      fail(verificationErrorString);
